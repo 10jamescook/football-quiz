ae7f0d87942fbf6a96beebaa823111a41aa57622
James <jamescook_10@hotmail.co.uk>
2026-04-01 22:06:23 +0100

    Added back and next buttons and a score keeper

--- src/App.js
+++ src/App.js
@@ -8,25 +8,60 @@ import quizData from "./Data/questions";
 function App() {
   const [started, setStarted] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
   
   const handleAnswer = (selected) => {
-    setCurrentQuestion((prev) => prev + 1);
-  };
+    const correctAnswer = quizData[currentQuestion].answers;
+
+    if (selected === correctAnswer) {
+      setScore((prev) => prev + 1);
+    };
+
+    const nextQuestion = () => {
+      if (currentQuestion < quizData.length - 1) {
+        setCurrentQuestion((prev) => prev + 1);
+      }
+    };
+
+    const prevQuestion = () => {
+      if (currentQuestion > 0) {
+        setCurrentQuestion((prev) => prev - 1);
+      }
+    };
 
   return ( 
     <div>
       {!started ? (
         <Welcome onStart={() => setStarted(true)} />
       ) : currentQuestion < quizData.length ? (
+        <>
+          <h2>Score: {score} </h2>
+
         <Quiz
         data = {quizData[currentQuestion]} 
         onAnswer={handleAnswer}
         />
+
+        <button 
+        onClick={prevQuestion}
+        disabled={currentQuestion === 0}
+        >
+          Previous
+        </button>
+
+        <button
+        onClick={nextQuestion}
+        disabled={currentQuestion === quizData.length - 1}
+        >
+          Next
+        </button>
+        </>     
       ) : (
         <h2>Quiz Completed! Thanks for playing.</h2>
       )}
     </div>
   );
+}
 }
 
 
